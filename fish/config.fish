982996d46537dc301f1a3607d980dbdbc7aaa7e8
37signalsS <misha-eliseyev@mail.ru>
2024-12-05 22:00:31 +0300

    add

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,8 @@ alias add='git add .'
 
 alias push='git push'
 
+alias ssh='kitten ssh'
+
 alias vpnon='sudo systemctl start tailscaled && sleep 3 && sudo tailscale up'
 
 alias vpnoff='sudo systemctl stop tailscaled && sleep 3 && sudo tailscale down' 
